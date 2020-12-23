0096ff57d4ab9569dcb3bbe7e32c8d73c017e4be
Carlve <ecv@ebbesen.org>
2020-12-23 18:09:25 +0100

    Første commit til dette prosjektet, har kun startet med npm create react app.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reacts.org"
           target="_blank"
           rel="noopener noreferrer"
         >
